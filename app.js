3cabbe918088583813a4afed934a53ce3f0c4381
miss-f-g <fatemeghafari98@gmail.com>
2022-08-03 14:41:55 +0430

    7.add enterAnimation func

--- app.js
+++ app.js
@@ -5,45 +5,79 @@ const tlEnter = gsap.timeline({
   defaults: { duration: 0.75, ease: "Power2.easeOut" },
 });
 
-
-
 const leaveAnimation = (current, done) => {
   const product = current.querySelector(".image-container");
   const text = current.querySelector(".showcase-text");
   const circles = current.querySelectorAll(".circle");
   const arrow = current.querySelector(".showcase-arrow");
 
-
-  return(
-    tlLeave.fromTo(arrow,{ opacity: 1, y: 0 },{ opacity: 0, y: 50, onComplete: done }),
-    tlEnter.fromTo(product, { y: -100, opacity: 0 }, { y: 0, opacity: 1 }, "<"),
-    tlEnter.fromTo(text, { y: 100, opacity: 0 }, { opacity: 1, y: 0 }, "<"),
-  )
-
-
-
+  return (
+    tlLeave.fromTo(
+      arrow,
+      { opacity: 1, y: 0 },
+      { opacity: 0, y: 50, onComplete: done }
+    ),
+    tlLeave.fromTo(product, { y: 0, opacity: 1 }, { y: 100, opacity: 0 }, "<"),
+    tlLeave.fromTo(text, { y: 0, opacity: 1 }, { opacity: 0, y: 100 }, "<"),
+    tlLeave.fromTo(
+      circles,
+      { y: 0, opacity: 1 },
+      {
+        y: -200,
+        opacity: 0,
+        stagger: 0.15,
+        ease: "back.out(1.7)",
+        duration: 1,
+      },
+      "<"
+    )
+  );
 };
 
-
+const enterAnimation = (current, done, gradient) => {
+  const product = current.querySelector(".image-container");
+  const text = current.querySelector(".showcase-text");
+  const circles = current.querySelectorAll(".circle");
+  const arrow = current.querySelector(".showcase-arrow");
+  return (
+    tlEnter.fromTo(
+      arrow,
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, onComplete: done }
+    ),
+    tlEnter.to("body", { background: gradient }, "<"),
+    tlEnter.fromTo(product, { y: -100, opacity: 0 }, { y: 0, opacity: 1 }, "<"),
+    tlEnter.fromTo(text, { y: 100, opacity: 0 }, { opacity: 1, y: 0 }, "<"),
+    tlEnter.fromTo(
+      circles,
+      { y: -200, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.15,
+        ease: "back.out(1.7)",
+        duration: 1,
+      },
+      "<"
+    )
+  );
+};
 
 barba.init({
-    preventRunning: true,
-    transitions: [
-      //showcase transitions
-      {
-        name: "default",
-        leave(data) {
-          const done = this.async();
-          let current = data.current.container;
-          leaveAnimation(current, done);
-        },
-        enter(data) {
-          const done = this.async();
-          let next = data.next.container;
-          gsap.fromTo(next, { opacity: 0 }, { opacity: 1, duration: 1 }, onComplete: done)
-
-        },
+  preventRunning: true,
+  transitions: [
+    //showcase transitions
+    {
+      name: "default",
+      leave(data) {
+        const done = this.async();
+        let current = data.current.container;
+        leaveAnimation(current, done);
       },
-    
-    ],
-  });
+      enter(data) {
+        const done = this.async();
+        let next = data.next.container;
+      },
+    },
+  ],
+});

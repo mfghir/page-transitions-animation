5d7f6a64dda0e6b072a81b0505a8a8087f19b010
miss-f-g <fatemeghafari98@gmail.com>
2022-08-03 13:11:45 +0430

    5.add timelines

---
+++ app.js
@@ -0,0 +1,7 @@
+const tlLeave = gsap.timeline({
+    defaults: { duration: 0.75, ease: "Power2.easeOut" },
+  });
+  const tlEnter = gsap.timeline({
+    defaults: { duration: 0.75, ease: "Power2.easeOut" },
+  });
+  
